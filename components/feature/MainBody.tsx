f8f095edcd9ec4573777b46ce2b33cb6521feada
Agyare <kofiagyare0000@gmail.com>
2023-08-03 17:57:29 +0000

    initial

---
+++ components/feature/MainBody.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaSistrix } from "react-icons/fa";
+
+const MainBody = () => {
+  return (
+    <div className="mainbody">
+      <div className="mainbody_content">
+        <h1>Find a Recipe</h1>
+
+        <div className="search_section">
+          <button>
+            <FaSistrix size={15} />
+          </button>
+          <input />
+        </div>
+        <p className="">
+          Unleash Your Inner Chef! Discover a world of mouthwatering recipes .
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MainBody;
